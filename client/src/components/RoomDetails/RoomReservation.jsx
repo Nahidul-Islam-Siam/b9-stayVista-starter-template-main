2d653c6bf10021226860ee8083d05bc967dbfd8f
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-05-31 21:05:19 +0600

    updating

--- client/src/components/RoomDetails/RoomReservation.jsx
+++ client/src/components/RoomDetails/RoomReservation.jsx
@@ -1,49 +1,66 @@
-import PropTypes from 'prop-types'
-import Button from '../Shared/Button/Button'
-import { useState } from 'react';
-import { DateRange } from 'react-date-range';
+import PropTypes from "prop-types";
+import Button from "../Shared/Button/Button";
+import { useState } from "react";
+import { DateRange } from "react-date-range";
+import { differenceInCalendarDays } from "date-fns";
 const RoomReservation = ({ room }) => {
+  console.log("start date--->", new Date(room.from).toLocaleDateString());
+  console.log("End date--->", new Date(room.to).toLocaleDateString());
   const [state, setState] = useState([
     {
-      startDate: new Date(),
-      endDate: null,
-      key: 'selection'
-    }
+      startDate: new Date(room.from),
+      endDate: new Date(room.to),
+      key: "selection",
+    },
   ]);
+// totalDays * price = totalPrice
+const totalPrice = parseInt(
+  differenceInCalendarDays(new Date(room.to),new Date(room.from)) * room?.price
+)
+console.log(totalPrice);
+
+
   return (
-    <div className='rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white'>
-      <div className='flex items-center gap-1 p-4'>
-        <div className='text-2xl font-semibold'>$ {room?.price}</div>
-        <div className='font-light text-neutral-600'>night</div>
+    <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
+      <div className="flex items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {room?.price}</div>
+        <div className="font-light text-neutral-600">/night</div>
       </div>
       <hr />
-      <div className='flex justify-center'>
+      <div className="flex justify-center">
         {/* Calender */}
         <DateRange
-        showDateDisplay={false}
-        rangeColors={['#F6536D']}
-  editableDateInputs={true}
-  onChange={item => setState([item.selection])}
-  moveRangeOnFirstSelection={false}
-  ranges={state}
-/>
-        
-        </div>
-      <hr />
-      <div className='p-4'>
-        <Button label={'Reserve'} />
+          showDateDisplay={false}
+          rangeColors={["#F6536D"]}
+          editableDateInputs={true}
+          onChange={(item) =>
+            setState([
+              {
+                startDate: new Date(room.from),
+                endDate: new Date(room.to),
+                key: "selection",
+              },
+            ])
+          }
+          moveRangeOnFirstSelection={false}
+          ranges={state}
+        />
       </div>
       <hr />
-      <div className='p-4 flex items-center justify-between font-semibold text-lg'>
+      <div className="p-4">
+        <Button label={"Reserve"} />
+      </div>
+      <hr />
+      <div className="p-4 flex items-center justify-between font-semibold text-lg">
         <div>Total</div>
-        <div>${room?.price}</div>
+        <div>${totalPrice}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 RoomReservation.propTypes = {
   room: PropTypes.object,
-}
+};
 
-export default RoomReservation
+export default RoomReservation;
